2544217947b170e170072df2e0a494c591425e26
bhlindemann <bh@lindemannrock.com>
2026-04-20 15:49:18 +0400

    docs(config): add template for Formie SAP Integration settings
    This commit introduces a new config.php file as a template for the Formie SAP Integration settings. It provides guidance on how to override default settings by copying it to 'craft/config' as 'formie-sap-integration.php'.

---
+++ src/config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Formie SAP Integration config.php
+ *
+ * This file exists only as a template for the Formie SAP Integration settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'formie-sap-integration.php'
+ * and make your changes there to override default settings.
+ *
+ * @since 3.3.1
+ */
+
+return [
+    // Global settings
+    '*' => [
+        // ========================================
+        // GENERAL SETTINGS
+        // ========================================
+
+        /**
+         * Plugin name shown in the Control Panel.
+         */
+        // 'pluginName' => 'Formie SAP Integration',
+    ],
+];
